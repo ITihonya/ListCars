45181327542a7dde1a4aa7c1f57c1c9d1dd375c1
ITihonya <bez-kupyr@mail.ru>
2020-03-31 17:17:14 +0300

    Commit 1

---
+++ src/App.js
@@ -0,0 +1,126 @@
+//import React from 'react';
+import './App.scss';
+import Car from './Car/index'
+import React, {Component} from 'react';
+
+class App extends Component {
+
+    constructor(props) {
+        console.log('App constructor')
+        super(props);
+        this.state={
+            cars: [
+                {name :'Ford', year : 2018},
+                {name :'Audi', year : 2016},
+                {name :'Mazda', year : 2010},
+            ],
+            pageTitle: 'React components',
+            showCars: false
+        }
+    }
+
+    onChangeName (name,index) {
+        const car = this.state.cars[index]
+        car.name = name
+        const newCars = [...this.state.cars]
+        newCars[index] = car
+        this.setState({
+            cars : newCars
+        })
+    }
+    deleteHandler (index) {
+        const newCars = this.state.cars.concat()
+        newCars.splice(index,1)
+        this.setState({cars : newCars})
+    }
+    toggleTitleHandler = () => {
+        this.setState({
+            showCars: !this.state.showCars
+        })
+    }
+
+    componentWillMount() {
+        //формируется при инициализации
+        console.log('App componentWillMount')
+    }
+    componentDidMount() {
+        //сообщает что  весь html готов и его можно показывать
+        console.log('App componentDidMount')
+    }
+
+
+    render() {
+        //формирует конечный jsx и html
+        console.log('App render')
+        const divStyle = {
+            textAlign:'center'
+            /*а можно записать и по старинке с кавычками,тоже будет работать text-align:'center'*/
+        }
+         let cars = null
+        if(this.state.showCars){
+            cars = this.state.cars.map((car, index) => {
+                return (
+                    <Car
+                        key={index}
+                        name={car.name}
+                        year={car.year}
+                        onDelete={this.deleteHandler.bind(this,index)}
+                        onChangeName={(event) => this.onChangeName(event.target.value,index)}
+                    />
+                )
+            })
+        }
+        return (
+            /*style={{color:'red',fontSize:'20px'а можно записать и по старинке с кавычками,тоже будет работать'font-size':20px}}*/
+            <div style={divStyle}>
+                { /*<h1>{this.state.pageTitle}</h1>*/}
+                <h1>{this.props.title}</h1>
+
+                <button className={'AppButton'}
+                    onClick={this.toggleTitleHandler}
+                >Toggle Title</button>
+                <div style={{
+                    width: '400px',
+                    margin: 'auto',
+                    padding: '20px'
+
+
+                }}>
+                    { cars }
+                </div>
+
+                {/*Простой пример вывода списка!А выще тот который нужно использовать!!!<Index*/}
+                {/*    name={cars[0].name}*/}
+                {/*    year={cars[0].year}*/}
+                {/*    onChangeTitle={this.changeTitleHandler.bind(this, cars[0].name)}*/}
+                {/*>*/}
+                {/*</Index>*/}
+                {/*<Index*/}
+                {/*    name={cars[1].name}*/}
+                {/*    year={cars[1].year}*/}
+                {/*    onChangeTitle={() => this.changeTitleHandler(cars[1].name)}*/}
+                {/*>*/}
+                {/*</Index>*/}
+                {/*<Index*/}
+                {/*    name={cars[2].name}*/}
+                {/*    year={cars[2].year}*/}
+                {/*    onChangeTitle={() => this.changeTitleHandler(cars[2].name)}*/}
+                {/*>*/}
+                {/*</Index>*/}
+            </div>
+        );
+        // return React.createElement(
+        //     'div',
+        //     {
+        //       className : 'App'
+        //     },
+        //     React.createElement(
+        //         'h1',
+        //         null,
+        //         'Hello world!'
+        //     )
+        // )
+    }
+}
+export default App;
+
---
+++ src/Car/index.js
@@ -0,0 +1,175 @@
+import React from 'react';
+import './Car.scss'
+//import Radium from 'radium'
+
+/*ЛУЧШЕ ВОТ ТАК ЧЕМ НИЖЕ!!!const Car = props =>{
+    const inputClasses = ['input']
+
+    if(this.props.name !== ''){
+        inputClasses.push('green')
+    }
+    else {
+        inputClasses.push('red')
+    }
+    if(this.props.name.length > 4){
+        inputClasses.push('bold')
+    }
+    const style = {
+        border: '1px solid #ccc',
+        boxShadow: '0 4px 5px 0 rgba(0, 0, 0,.14)',
+        ':hover': {
+            border: '1px solid #aaa',
+            boxShadow: '0 4px 15px 0 rgba(0, 0, 0, .25)',
+            cursor: 'pointer'
+        }
+    }
+    return (
+        <div className="Car" style={style}>
+            <h3>Car name: {this.props.name}</h3>
+            <p>Year: <strong>{this.props.year}</strong></p>
+            <input
+                type="text"
+                onChange={this.props.onChangeName}
+                value={this.props.name}
+                className={inputClasses.join(' ')}
+            />
+            <button onClick={this.props.onDelete}>Delete</button>
+        </div>
+    )
+}*/
+class Car extends React.Component{
+
+    componentWillReceiveProps(nextProps, nextContext) {
+        console.log('Car componentWillReceiveProps',nextProps,nextContext)
+    }
+
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        console.log('Car shouldComponentUpdate',nextProps,nextState,nextContext)
+        return nextProps.name.trim()!== this.props.name.trim()
+    }
+
+    componentWillUpdate(nextProps, nextState, nextContext) {
+        console.log('Car componentWillUpdate',nextProps,nextState,nextContext)
+    }
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        console.log('Car getDerivedStateFromProps', nextProps, prevState)
+        return prevState
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('Car componentDidUpdate',prevProps,prevState,snapshot)
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('Car getSnapshotBeforeUpdate')
+    }
+
+    componentWillUnmount() {
+        console.log('Car componentWillUnmount')
+    }
+
+    render (){
+        console.log('Car render')
+        const inputClasses = ['input']
+
+        if(this.props.name !== ''){
+            inputClasses.push('green')
+        }
+        else {
+            inputClasses.push('red')
+        }
+        if(this.props.name.length > 4){
+            inputClasses.push('bold')
+        }
+        const style = {
+            border: '1px solid #ccc',
+            boxShadow: '0 4px 5px 0 rgba(0, 0, 0,.14)',
+            ':hover': {
+                border: '1px solid #aaa',
+                boxShadow: '0 4px 15px 0 rgba(0, 0, 0, .25)',
+                cursor: 'pointer'
+            }
+        }
+        return (
+            <div className="Car" style={style}>
+                <h3>Car name: {this.props.name}</h3>
+                <p>Year: <strong>{this.props.year}</strong></p>
+                <input
+                    type="text"
+                    onChange={this.props.onChangeName}
+                    value={this.props.name}
+                    className={inputClasses.join(' ')}
+                />
+                <button onClick={this.props.onDelete}>Delete</button>
+            </div>
+        )
+    }
+}
+//можно так!!!
+// function car() {
+//     return (
+//         <div>This is car components</div>
+//     )
+// }
+
+//Можно покороче!!!
+// const car = () => {
+//     return (
+//         <div>This is car components</div>
+//     )
+// }
+
+//А можно и вот так
+//const car = () => <div>This is car components</div>
+
+//а если возвращаем не один div а ещё что...то уже с круглыми скобками
+// const car = () => (
+//     <div>
+//         This is car components
+//         <strong>test</strong>
+//     </div>
+// )
+
+//export default car
+
+//а можно покороче
+// export default () => (
+//     <div>
+//        This is car components
+//        <strong>test</strong>
+//      </div>
+// )
+
+//Продолжаем!!!Будем делать так!!!!!!!
+/*если 1+1 не обернуть в фиг.скобки то это будет обычный
+текст html,если же хотим динамики(т.е. преобразовать в js)
+добавляем фиг.скобки*/
+// export default () => (
+//     <div>
+//        <p>This is car components</p>
+//         <p><strong>{1+1}</strong></p>
+//         <p><strong>Number : {Math.round(Math.random() *100)}</strong></p>
+//      </div>
+// )
+
+//11 Урок 2,6
+/*если в стрелочной функции мы принимаем один параметр
+например props то круглые скобки можно не писать*/
+/*Если в компоненте нужно передать ещё какой нибудь контент например
+цвет, то тогда в App.js указываем вот так:
+    <Car name="Audi" year={2016}>
+        <p style={{color:'red'}}>COLOR</p>
+    </Car>
+а здесь index.js обращаемся {props.children}
+<div>
+    <h3>Car name: {props.name}</h3>
+    <p>Year: <strong>{props.year}</strong></p>
+    {props.children}
+</div>*/
+
+
+export default Car
+
+
+
+
